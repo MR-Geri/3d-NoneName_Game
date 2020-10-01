93662626eea4a8591f972898fc8bdac85a3bb1e9
MR-Geri <ilya.kamckine@ya.ru>
2020-10-01 21:38:52 +0300

    png

--- ray_casting.py
+++ ray_casting.py
@@ -14,7 +14,9 @@ def ray_casting(sc, pos, angle):
     cur_angle = angle - HALF_FOV
     for ray in range(NUM_RAYS):
         sin = math.sin(cur_angle)
+        sin = sin if sin else 0.000001
         cos = math.cos(cur_angle)
+        cos = cos if cos else 0.000001
         x, dx = (xm + BLOCK_TITLE, 1) if cos >= 0 else (xm, -1)
         for i in range(0, WIDTH, BLOCK_TITLE):
             w_depth = (x - x0) / cos
@@ -29,9 +31,9 @@ def ray_casting(sc, pos, angle):
             if mapping(x, y + dy) in world_map:
                 break
             y += dy * BLOCK_TITLE
-        depth = min(w_depth, h_depth)
+        #
+        depth = w_depth if w_depth < h_depth else h_depth
         depth *= math.cos(angle - cur_angle)
-        depth = max(depth, 0.00001)
         height = min(int(COEFFICIENT / depth), HEIGHT * 2)
         color = 255 / (1 + depth * depth / 100000)
         pygame.draw.rect(
